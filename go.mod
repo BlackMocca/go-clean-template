06bd9d086783655267b4fb1e7818917e7d1d009e
Teeradet <huag1234@hotmail.com>
2019-08-22 17:35:40 +0700

    🐱add init app

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/go-clean-template
+
+go 1.12
+
+require github.com/labstack/echo/v4 v4.1.10 // indirect
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":3000"))
+}
