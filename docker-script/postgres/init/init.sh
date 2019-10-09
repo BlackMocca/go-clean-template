f2fef24ab4e8c74b71d6d62e45de575682020909
Teeradet-Huag <teeradet.huag@gmail.com>
2019-10-09 20:03:01 +0700

    🐱add postgres docker

---
+++ docker-script/postgres/init/init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -o errexit
+
+main() {
+  echo "CREATING DATABASE $POSTGRES_DB_DEVELOPMENT AND $POSTGRES_DB_TEST"
+  create_databases
+
+  echo "CREATING UUID-OSSP EXTENSIONS" 
+  create_extensions
+}
+
+create_databases() {
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "Create database $POSTGRES_DB_DEVELOPMENT"
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "Create database $POSTGRES_DB_TEST"
+}
+
+create_extensions() {
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname="$POSTGRES_DB_DEVELOPMENT" <<-EOSQL
+     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+EOSQL
+
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname="$POSTGRES_DB_TEST" <<-EOSQL
+     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+EOSQL
+}
+
+main "$@"
