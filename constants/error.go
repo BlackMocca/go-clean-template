c2648b25b924687fa8a3791f5ef39679ebd8c8ff
Teeradet Phondetparinya <teeradet.phon@innovasive.co.th>
2022-08-25 16:44:02 +0700

    🐱rebrand example

--- constants/error.go
+++
@@ -1,7 +0,0 @@
-package constants
-
-const (
-	ERROR_DUPLICATE_EMAIL = "pq: duplicate key value violates unique constraint \"users_email_key\""
-
-	ERROR_DUPLICATE_EMAIL_MESSAGE = "email was duplicate"
-)
--- service/user/http.go
+++
@@ -1,9 +0,0 @@
-package user
-
-import "github.com/labstack/echo/v4"
-
-type UserHandler interface {
-	FetchAll(c echo.Context) error
-	FetchOneByUserId(c echo.Context) error
-	Create(c echo.Context) error
-}
